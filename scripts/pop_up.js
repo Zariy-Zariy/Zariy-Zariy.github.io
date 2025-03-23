a1fb052f9a05f4f8aeaaaed990a62572466d323b
Zariy-Zariy <zaroui.iyad@hotmail.com>
2025-03-23 14:16:31 +0100

    Finished the pop-up feature.

--- scripts/pop_up.js
+++ scripts/pop_up.js
@@ -1,3 +1,10 @@
-function open_pop_up(){
-    
+function open_pop_up(button){
+    let id = button.dataset.popid;
+    let pop_up = document.querySelector(`#popID-${id}`);
+    pop_up.classList.add("show");
+}
+
+function close_pop_up(button){
+    let pop_up = button.parentElement;
+    pop_up.classList.remove("show");
 }
